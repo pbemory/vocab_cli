e2b53ecce3f4597c5e8cc3c846aa429382ee9398
sirjohnstonsmith <pbemory@users.noreply.github.com>
2022-11-11 16:33:03 -0600

    housekeeping pycharm + directory setup

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
-import csv, os, random
+import csv, os, random, sys
 import config
 from wordnik_api_client import WordnikClient
 
-wordnik_client = WordnikClient()
+'''
+main's tasks:
+(1) get our wordbank; 
+(2) give a status report (based on read_history()); 
+(3) run the app.
+'''
+def main():
+    args = sys.argv[1:]
+    if len(args) == 2 and args[0] == '-wb':
+        word_bank_path = args[1]
+    else:
+        word_bank_path = 'word_bank.csv'
 
-results = wordnik_client.get_def("overhang")
+'''
+read_history:
+--determine last time app ran by checking last save date
+--tell us how many words we've learned this week (starting from Sunday)
+--don't overcomplicate--should just be since last Sunday, that 's a refactor
+'''
+def read_history():
+    pass
 
+
+def run_vocab_exercise():
+    wordnik_client = WordnikClient()
+    results = wordnik_client.get_def("overhang")
+
+
+def save():
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- wordnik_api_client.py
+++ wordnik_api_client.py
@@ -1,15 +1,14 @@
 import requests
 import config
 
+
 class WordnikClient:
 
-  def __init__(self,api_key=config.api_key):
-    self.api_key = api_key
-    
+    def __init__(self, api_key=config.api_key):
+        self.api_key = api_key
 
-  def get_def(self,word:str)->dict:
-    url = f"https://api.wordnik.com/v4/word.json/{word}/definitions?limit=1&includeRelated=false&useCanonical=false&includeTags=false"
-    params = {'api_key':self.api_key}
-    response = requests.get(url,params=params).json()
-    return response
-
+    def get_def(self, word: str) -> dict:
+        url = f"https://api.wordnik.com/v4/word.json/{word}/definitions"
+        params = {'api_key': self.api_key, 'limit': '1'}
+        response = requests.get(url, params=params).json()
+        return response
