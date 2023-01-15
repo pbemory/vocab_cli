6be45bdd286f51d8d767525f4c38e18a29ef5ceb
sirjohnstonsmith <pbemory@users.noreply.github.com>
2023-01-14 20:02:54 -0600

    building out run_vocab_exercise

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import csv, os, random, sys
-import config
-from wordnik_api_client import WordnikClient
+import csv, os, random, sys 
+from wordnik_client import WordnikClient
 
 '''
 main's tasks:
-(1) get our wordbank; 
-(2) give a status report (based on read_history()); 
-(3) run the app.
+(1) give a status report (based on read_history); 
+(2) run the app.
 '''
 def main():
     args = sys.argv[1:]
@@ -14,25 +12,61 @@ def main():
         word_bank_path = args[1]
     else:
         word_bank_path = 'word_bank.csv'
+    read_history()
+    run_vocab_exercise(word_bank_path)
 
 '''
 read_history:
 --determine last time app ran by checking last save date
 --tell us how many words we've learned this week (starting from Sunday)
---don't overcomplicate--should just be since last Sunday, that 's a refactor
 '''
 def read_history():
     pass
 
+'''
 
-def run_vocab_exercise():
+'''
+def run_vocab_exercise(word_bank_path: str):
+    leveled_up_words = 0
     wordnik_client = WordnikClient()
-    results = wordnik_client.get_def("overhang")
+    with open(word_bank_path,'r') as csvfile:
+        #lines = csvfile.readlines()
+        reader = csv.reader(csvfile)
+        new_word_bank = open(word_bank_path.replace('.csv','_temp'),'w')
+        writer = csv.writer(new_word_bank)     
+        quit = False
+        for row in reader:
+            if quit is True:
+                writer.writerow(row)
+            else:
+                if int(row[2].strip()) == 1:
+                    writer.writerow(row)
+                else:
+                    word = row[0].strip()
+                    try:
+                        word_results = wordnik_client.get_word_def_and_ex(word)
+                        word_def = word_results['text']
+                        word_prompt = input(f"What word means '{word_def}'? ")
+                        confirm_answer_prompt = input(f"Was your word '{word}'? ")
+                        if confirm_answer_prompt == 'y':
+                            row = level_up_word(row)
+                            leveled_up_words += 1
+                            print(f"'{word}' leveled up! {leveled_up_words} word{'s'[:leveled_up_words^1]} learned so far ...")
+                        writer.writerow(row)
+                        if confirm_answer_prompt == 'q':
+                            quit = True
+                    except:
+                        print(f"Wordnik couldn't find a definition for '{word}'")
+        new_word_bank.close()
+    csvfile.close()
+    os.remove(word_bank_path)
+    os.rename(word_bank_path.replace('.csv','_temp'),word_bank_path)
 
-
-def save():
-    pass
-
+def level_up_word(csv_row:list) -> list:
+    word_level_number = int(csv_row[2])
+    word_level_number += 1
+    new_row = [csv_row[0], csv_row[1], " " + str(word_level_number)]
+    return new_row
 
 if __name__ == '__main__':
     main()
--- wordnik_client.py
+++ wordnik_client.py
@@ -1,4 +1,5 @@
 import requests
+import json
 import config
 
 
@@ -7,8 +8,10 @@ class WordnikClient:
     def __init__(self, api_key=config.api_key):
         self.api_key = api_key
 
-    def get_def(self, word: str) -> dict:
+    def get_word_def_and_ex(self, word: str) -> dict:
         url = f"https://api.wordnik.com/v4/word.json/{word}/definitions"
         params = {'api_key': self.api_key, 'limit': '1'}
         response = requests.get(url, params=params).json()
-        return response
+        #response is technically a list, but we only want the first item
+        return response[0]
+
