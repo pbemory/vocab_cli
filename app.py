68353ebb4e6a893e2b76ebd45f58b3116ad303df
pbemory <pbemory@users.noreply.github.com>
2023-02-13 16:45:29 -0600

    Linting and small fix within read_history.

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-import csv, os, random, sys
+import csv
+import os
+import random
+import sys
 from datetime import datetime, date, timedelta
 import asyncio
 from wordnik_client import WordnikClient
@@ -14,6 +17,7 @@ def main():
     words_learned_this_week = read_history()
     run_vocab_exercise(word_bank_path, words_learned_this_week)
 
+
 def read_history() -> int:
     """status_db.csv stores words learned this week (since Sunday) + 
     stores last time run_vocab_exercise saved, 
@@ -24,17 +28,20 @@ def read_history() -> int:
         most_recent_sunday = date.today()
     else:
         most_recent_sunday = date.today() - timedelta(date.weekday(datetime.now())+1)
-    with open('status_db.csv','r') as status_db:
+    with open('status_db.csv', 'r', encoding='UTF-8') as status_db:
         db_reader = csv.reader(status_db)
         last_checked_sunday = date.fromisoformat(next(db_reader)[1])
         if most_recent_sunday > last_checked_sunday:
             words_learned_this_week = 0
+            next(db_reader)
         else:
             words_learned_this_week = int(next(db_reader)[1])
         words_remaining = int(next(db_reader)[1])
     status_db.close()
-    print(f"{words_learned_this_week} word{'s'[:words_learned_this_week^1]} learned this week of {words_remaining} word{'s'[:words_remaining^1]} remaining...")
+    print(
+        f"{words_learned_this_week} word{'s'[:words_learned_this_week^1]} learned this week of {words_remaining} word{'s'[:words_remaining^1]} remaining...")
     return words_learned_this_week
+
 
 def run_vocab_exercise(word_bank_path: str, words_learned_this_week: int):
     """Read from the existing word bank, while creating a new, writable word bank. 
@@ -44,17 +51,16 @@ def run_vocab_exercise(word_bank_path: str, words_learned_this_week: int):
     """
     leveled_up_words = 0
     words_left = 0
-    with open(word_bank_path,'r') as csvfile:
+    with open(word_bank_path, 'r', encoding='UTF-8') as csvfile:
         rows = csvfile.readlines()
-        total_word_count = len(rows)
-        #print(total_word_count)
         random.shuffle(rows)
         reader = csv.reader(rows)
-        new_word_bank = open(word_bank_path.replace('.csv','_temp'),'w')
-        writer = csv.writer(new_word_bank)     
-        quit = False
+        new_word_bank = open(word_bank_path.replace(
+            '.csv', '_temp'), 'w', encoding='UTF-8')
+        writer = csv.writer(new_word_bank)
+        user_quit = False
         for row in reader:
-            if quit is True:
+            if user_quit is True:
                 writer.writerow(row)
                 if int(row[1].strip()) == 0:
                     words_left += 1
@@ -64,51 +70,67 @@ def run_vocab_exercise(word_bank_path: str, words_learned_this_week: int):
                 else:
                     word = row[0].strip()
                     try:
-                        word_result = asyncio.run(WordnikClient().get_word_definition_and_example(word))
-                        word_prompt = input(f"What word means '{word_result.definition}'? ")
+                        word_result = asyncio.run(
+                            WordnikClient().get_word_definition_and_example(word))
+                        word_prompt = input(
+                            f"What word means '{word_result.definition}'? ")
                         if word_prompt == 'q':
                             writer.writerow(row)
                             words_left += 1
-                            quit = True
-                        if quit is False:
-                            print(f"****\nExample: '{word_result.example}'\n****")
+                            user_quit = True
+                        if user_quit is False:
+                            print(
+                                f"****\nExample: '{word_result.example}'\n****")
                             if word_prompt == word:
                                 row = level_up_word(row)
                                 leveled_up_words += 1
                                 words_learned_this_week += 1
-                                print(f"'{word}' leveled up! {leveled_up_words} word{'s'[:leveled_up_words^1]} learned so far ...\n****")
+                                print(
+                                    f"'{word}' leveled up! {leveled_up_words} word{'s'[:leveled_up_words^1]} learned so far ...\n****")
                             else:
                                 words_left += 1
-                                print(f"'{word_prompt}' did not match '{word}'.\n****")
+                                print(
+                                    f"'{word_prompt}' did not match '{word}'.\n****")
                             writer.writerow(row)
-                    except Exception as e:
+                    except Exception as exc:
                         writer.writerow(row)
                         words_left += 1
                         print(f"Something went wrong for '{word}'")
-                        print("Exception: " + str(e))
+                        print("Exception: " + str(exc))
         new_word_bank.close()
     csvfile.close()
     save(words_learned_this_week, words_left)
     os.remove(word_bank_path)
-    os.rename(word_bank_path.replace('.csv','_temp'),word_bank_path)
+    os.rename(word_bank_path.replace('.csv', '_temp'), word_bank_path)
+
 
 def save(words_learned_this_week: int, words_left: int):
+    """
+    Save progress to status_db.csv
+    """
     if date.weekday(datetime.now()) == 6:
         most_recent_sunday = date.today()
     else:
         most_recent_sunday = date.today() - timedelta(date.weekday(datetime.now())+1)
-    with open('status_db.csv','w') as status_db:
+    with open('status_db.csv', 'w', encoding='UTF-8') as status_db:
         writer = csv.writer(status_db)
-        writer.writerow(["Date of last checked Sunday", str(most_recent_sunday)])
-        writer.writerow(["Words learned since Sunday", str(words_learned_this_week)])
+        writer.writerow(
+            ["Date of last checked Sunday", str(most_recent_sunday)])
+        writer.writerow(["Words learned since Sunday",
+                        str(words_learned_this_week)])
         writer.writerow(["Words remaining", str(words_left)])
     status_db.close()
 
-def level_up_word(csv_row:list) -> list:
+
+def level_up_word(csv_row: list) -> list:
+    """
+    Increment word score column when word is correct in run_vocab_exercise
+    """
     word_level_number = int(csv_row[1])
     word_level_number += 1
     new_row = [csv_row[0], " " + str(word_level_number)]
     return new_row
+
 
 if __name__ == '__main__':
     main()
--- wordnik_client.py
+++ wordnik_client.py
@@ -2,23 +2,28 @@ import config
 import asyncio
 import aiohttp
 
+
 class WordnikResult:
+    """Used for storing results of wordnik fetches into consumable object."""
 
     def __init__(self, definition: str, example: str):
         self.definition = definition
         self.example = example
 
+
 class WordnikClient:
+    """Client for interacting with the wordnik API."""
 
     api_key = config.api_key
     wordnik_api_base_url = f"https://api.wordnik.com/v4/word.json/"
     params = {'api_key': api_key, 'limit': '3'}
 
     async def get_word_definition(self, word: str, session: aiohttp.ClientSession):
+        """Fetch definition of provided word. Try 3 of wordnik's dictionaries."""
         url = self.wordnik_api_base_url + f"{word}/definitions"
-        word_def = "No definition found."      
+        word_def = "No definition found."
         try:
-            definitions = await self.fetch(url,self.params,session)
+            definitions = await self.fetch(url, self.params, session)
             definition_key = 'text'
             error_key = 'message'
             if error_key in definitions:
@@ -28,15 +33,16 @@ class WordnikClient:
                     if definition_key in definition:
                         word_def = definition[definition_key]
                         break
-        except Exception as e:
-            word_def = "Exception: " + str(e)
+        except Exception as exc:
+            word_def = "Exception: " + str(exc)
         return word_def
 
     async def get_word_example(self, word: str, session: aiohttp.ClientSession):
+        """Fetch example of provided word. Try 3 of wordnik's dictionaries."""
         url = self.wordnik_api_base_url + f"{word}/examples"
         word_example = "No example found."
         try:
-            examples = await self.fetch(url,self.params,session)
+            examples = await self.fetch(url, self.params, session)
             example_key = 'text'
             error_key = 'message'
             if error_key in examples:
@@ -46,27 +52,29 @@ class WordnikClient:
                     if example_key in example:
                         word_example = example[example_key]
                         break
-        except Exception as e:
-            word_example = "Exception: " + str(e)
+        except Exception as exc:
+            word_example = "Exception: " + str(exc)
         return word_example
 
     async def get_word_definition_and_example(self, word: str) -> WordnikResult:
+        """Queue and execute definition + examples fetches. Store in WordnikResult object."""
         async with aiohttp.ClientSession() as session:
             tasks = []
-            tasks.append(asyncio.create_task(self.get_word_definition(word,session)))
-            tasks.append(asyncio.create_task(self.get_word_example(word,session)))
+            tasks.append(asyncio.create_task(
+                self.get_word_definition(word, session)))
+            tasks.append(asyncio.create_task(
+                self.get_word_example(word, session)))
             results = await asyncio.gather(*tasks)
-            wordnik_result = WordnikResult(definition=results[0],example=results[1])
+            wordnik_result = WordnikResult(
+                definition=results[0], example=results[1])
             return wordnik_result
 
-
     async def fetch(self, url: str, params: dict, session: aiohttp.ClientSession):
-        async with session.get(url=url,params=params) as response:
+        """Helper method of async request/response within session."""
+        async with session.get(url=url, params=params) as response:
             return await response.json()
 
-'''
-Code below for debugging.
-'''
-# wordnik_result = asyncio.run(WordnikClient.get_word_definition_and_example('parsimony'))
-# print(wordnik_result.definition)
-# print(wordnik_result.example)
+    # Code below for debugging:
+    # wordnik_result = asyncio.run(WordnikClient.get_word_definition_and_example('parsimony'))
+    # print(wordnik_result.definition)
+    # print(wordnik_result.example)
