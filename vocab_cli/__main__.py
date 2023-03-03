f4dcf43b8b29ed4a3e94fab0a61c5a1db205cd0b
pbemory <pbemory@users.noreply.github.com>
2023-03-03 01:28:49 -0600

    Separate main from parse_args

--- vocab_cli/__main__.py
+++ vocab_cli/__main__.py
@@ -2,14 +2,19 @@ from vocab_cli import app, __version__
 import sys
 import argparse
 
-def main():
-    """Entry point script for launching vocab_cli."""
+def parse_args():
+    """Parse args with argparse."""
     parser = argparse.ArgumentParser(description = 'cli tool for building vocab from a word bank.')
     parser.add_argument('-v','--version', action='version', version=f"Version {__version__}")
     parser.add_argument('-wb', help='specify a path to your word_bank.')
     args = parser.parse_args()
-    word_bank_path = args.wb
-    app.launch(word_bank_path)
+    return args
+
+
+def main():
+    """Launch app with args."""
+    args = parse_args()
+    app.launch(args.wb)
 
 
 if __name__ == '__main__':
