15e776d9e14b0689deffdb79d799e5fabbe55dc2
pbemory-job <pbemory@users.noreply.github.com>
2023-07-01 19:20:51 -0500

    Add ability to add words to word bank.

--- vocab_cli/__main__.py
+++ vocab_cli/__main__.py
@@ -1,3 +1,5 @@
+import os
+import csv
 import argparse
 from vocab_cli import app, __version__
 
@@ -7,6 +9,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description = 'cli tool for building vocab from a word bank.')
     parser.add_argument('-v','--version', action='version', version=f"Version {__version__}")
     parser.add_argument('-wb', help='specify a path to your word_bank.')
+    parser.add_argument('-a', help='Add words to the word bank as a list. E.g.: objurgate, mollify, globose')
     args = parser.parse_args()
     return args
 
@@ -14,8 +17,21 @@ def parse_args():
 def main():
     """Launch app with args."""
     args = parse_args()
-    app.launch(args.wb)
+    if (args.a != None):
+        add_words(args.a)
+    else:
+        app.launch(args.wb)
 
+def add_words(new_words):
+    """Add words to word bank."""
+    new_words_list = new_words.split(",")
+    word_bank_path = os.path.join(
+        os.path.dirname(__file__), 'word_bank.csv')
+    with open(word_bank_path, 'a', encoding='UTF-8') as csvfile:
+        writer = csv.writer(csvfile)
+        for word in new_words_list:
+            writer.writerow([word,0])
+    csvfile.close()
 
 if __name__ == '__main__':
     main()
